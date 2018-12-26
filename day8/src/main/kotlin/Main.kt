fe62e3ba8b852ec4525af5434e3817d6296b61e7
Mike Neilens <mike.neilens@gmail.com>
2018-12-26 10:41:22 +0000

    day 8 part 2

--- day8/src/main/kotlin/Main.kt
+++ day8/src/main/kotlin/Main.kt
@@ -5,11 +5,21 @@ fun main(args: Array<String>) {
 
     val pair = createNode(listOfData)
     val root = pair.first
-    println("sum of nodes is ${root.sum()}")
+    println("sum of nodes is ${root.sum()}") //44838
+    println("sum of nodes 2 is ${root.sum2()}")
+
 }
+
 class Node(val noOfChildNodes:Int, val noOfMetaDataEntries:Int, val childNodes:List<Node>, val metaDataEntries:List<Int>) {
     fun sum():Int = metaDataEntries.fold(0){acc, element -> acc + element} + this.childNodes.fold(0){acc,element -> acc + element.sum()}
 
+    fun sum2():Int = if (noOfChildNodes == 0) metaDataEntries.fold(0){ acc, element -> acc + element}
+                    else {
+                        metaDataEntries.fold(0){acc, metaDataEntry ->
+                            if ((metaDataEntry == 0) || metaDataEntry > noOfChildNodes) acc
+                            else acc + childNodes[metaDataEntry - 1].sum2()
+                        }
+                    }
 }
 
 fun createNode(listOfInts:List<Int>):Pair<Node,List<Int>> {
@@ -21,6 +31,7 @@ fun createNode(listOfInts:List<Int>):Pair<Node,List<Int>> {
         val remainingList = listOfInts.drop(2 + noOfMetaDataEntries)
         return Pair(node,remainingList)
     }
+
     var remainingList = listOfInts.drop(2)
     var listOfNodes = listOf<Node>()
     (1..noOfChildNodes).forEach {
@@ -31,8 +42,8 @@ fun createNode(listOfInts:List<Int>):Pair<Node,List<Int>> {
     val metaDataEntries = remainingList.subList(0,noOfMetaDataEntries)
     remainingList = remainingList.drop(noOfMetaDataEntries)
     return Pair(Node(noOfChildNodes,noOfMetaDataEntries, listOfNodes, metaDataEntries ),remainingList)
-}
 
+}
 
 fun readFile():List<String> {
     val lineList = mutableListOf<String>()
